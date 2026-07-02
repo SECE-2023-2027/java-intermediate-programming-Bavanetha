505f0e4cec3a2c555ba4b5e6f45b3304226e47be
Bavanetha M R <bavanethamr@gmail.com>
2026-07-02 10:00:15 +0530

    problem 9 completed

---
+++ Problem9/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        AllInOneDevice device = new AllInOneDevice();
+        device.printData();
+        device.scanData();
+    }   
+}
